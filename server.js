a77ff3d079b2162a87046cf912d92491599c0f98
Rahhhulll <rkrahulrk651@gmail.com>
2026-03-11 12:23:29 +0530

    "mail Config Update"

--- server.js
+++ server.js
@@ -60,7 +60,9 @@ const upload = multer({
 ========================= */
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
