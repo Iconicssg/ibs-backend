a64bbcb82c3894928f3ced6a45d3d3a8a936a874
Rahhhulll <rkrahulrk651@gmail.com>
2026-03-11 13:04:14 +0530

    "smtp config Changes"

--- server.js
+++ server.js
@@ -61,14 +61,16 @@ const upload = multer({
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
-
 /* =========================
    APPLY JOB API
 ========================= */
